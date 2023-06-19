d24a0e4179b9f96106666d909c31960ee231cc0e
eya <eiia.aariibii@gmail.com>
2023-06-19 09:47:02 +0100

    first commit

---
+++ Redux/action.js
@@ -0,0 +1,14 @@
+import { DECREMENT, INCREMENT } from "./actionType";
+export const increment = () => {
+    return {
+      type: INCREMENT
+    };
+  };
+  export const decrement = () => {
+    return {
+      type: DECREMENT
+    };
+  };
+  
+
+
